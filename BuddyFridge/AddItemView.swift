8f20b934d55554432ddfe57b2e618571bb704566
NicolaDiCrescenzo04 <nicoladicrescenzo04@gmail.com>
2025-11-25 21:39:01 +0100

    first functions added

---
+++ BuddyFridge/AddItemView.swift
@@ -0,0 +1,18 @@
+//
+//  AddItemView.swift
+//  BuddyFridge
+//
+//  Created by Nicola Di Crescenzo on 25/11/25.
+//
+
+import SwiftUI
+
+struct AddItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddItemView()
+}
---
+++ BuddyFridge/DataModels.swift
@@ -0,0 +1,8 @@
+//
+//  DataModels.swift
+//  BuddyFridge
+//
+//  Created by Nicola Di Crescenzo on 25/11/25.
+//
+
+import Foundation
